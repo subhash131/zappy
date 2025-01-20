dd550a5613252e28554b0d6c9ad49e0e1e48c731
subhash131 <subhashnayak131@gmail.com>
2025-01-20 21:34:37 +0530

    update services

--- src/components/landing-page/services.tsx
+++ src/components/landing-page/services.tsx
@@ -1,12 +1,74 @@
 import React from "react";
+import { BsGraphUpArrow } from "react-icons/bs";
 
 const Services = () => {
   return (
-    <div className="w-full min-h-screen flex items-center justify-start flex-col pt-10">
+    <div className="w-full min-h-screen flex items-center justify-start flex-col pt-10 gap-10 px-40">
       <h2 className="text-5xl text-center text-gradient font-semibold">
         Grow your business with <br /> our services
       </h2>
-      <div className="flex"></div>
+      <div className="flex flex-wrap gap-10 items-center justify-center text-[rgb(197,197,197)]">
+        <div className="w-72 h-44 dashed-border text-center flex items-center justify-center gap-4 flex-col ">
+          <BsGraphUpArrow size={30} className="text-gradient" />
+          <div className="flex flex-col gap-2">
+            <h4 className="text-xl font-semibold ">Conversion-Focused</h4>
+            <p className="text-xs text-[#7a7a7a]">
+              We help brands exceed $20M monthly by turning visitors into loyal
+              customers and accelerating growth.
+            </p>
+          </div>
+        </div>
+        <div className="w-72 h-44 dashed-border text-center flex items-center justify-center gap-4 flex-col ">
+          <BsGraphUpArrow size={30} className="text-gradient" />
+          <div className="flex flex-col gap-2">
+            <h4 className="text-xl font-semibold ">Conversion-Focused</h4>
+            <p className="text-xs text-[#7a7a7a]">
+              We help brands exceed $20M monthly by turning visitors into loyal
+              customers and accelerating growth.
+            </p>
+          </div>
+        </div>
+        <div className="w-72 h-44 dashed-border text-center flex items-center justify-center gap-4 flex-col ">
+          <BsGraphUpArrow size={30} className="text-gradient" />
+          <div className="flex flex-col gap-2">
+            <h4 className="text-xl font-semibold ">Conversion-Focused</h4>
+            <p className="text-xs text-[#7a7a7a]">
+              We help brands exceed $20M monthly by turning visitors into loyal
+              customers and accelerating growth.
+            </p>
+          </div>
+        </div>
+        <div className="w-72 h-44 dashed-border text-center flex items-center justify-center gap-4 flex-col ">
+          <BsGraphUpArrow size={30} className="text-gradient" />
+          <div className="flex flex-col gap-2">
+            <h4 className="text-xl font-semibold ">Conversion-Focused</h4>
+            <p className="text-xs text-[#7a7a7a]">
+              We help brands exceed $20M monthly by turning visitors into loyal
+              customers and accelerating growth.
+            </p>
+          </div>
+        </div>
+        <div className="w-72 h-44 dashed-border text-center flex items-center justify-center gap-4 flex-col ">
+          <BsGraphUpArrow size={30} className="text-gradient" />
+          <div className="flex flex-col gap-2">
+            <h4 className="text-xl font-semibold ">Conversion-Focused</h4>
+            <p className="text-xs text-[#7a7a7a]">
+              We help brands exceed $20M monthly by turning visitors into loyal
+              customers and accelerating growth.
+            </p>
+          </div>
+        </div>
+        <div className="w-72 h-44 dashed-border text-center flex items-center justify-center gap-4 flex-col ">
+          <BsGraphUpArrow size={30} className="text-gradient" />
+          <div className="flex flex-col gap-2">
+            <h4 className="text-xl font-semibold ">Conversion-Focused</h4>
+            <p className="text-xs text-[#7a7a7a]">
+              We help brands exceed $20M monthly by turning visitors into loyal
+              customers and accelerating growth.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
