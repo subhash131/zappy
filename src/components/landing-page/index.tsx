7cbee891d8f5a5d26352159aece93c9cd9d7176c
subhash131 <subhashnayak131@gmail.com>
2025-01-20 19:39:10 +0530

    add services section

--- src/components/landing-page/index.tsx
+++ src/components/landing-page/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./navbar";
 import Hero from "./hero";
+import Services from "./services";
 
 const LandingPage = () => {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center relative">
+    <div className="w-full min-h-screen flex items-center justify-center relative flex-col">
       <Navbar />
       <Hero />
+      <Services />
     </div>
   );
 };
---
+++ src/components/landing-page/services.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-start flex-col pt-10">
+      <h2 className="text-5xl text-center text-gradient font-semibold">
+        Grow your business with <br /> our services
+      </h2>
+      <div className="flex"></div>
+    </div>
+  );
+};
+
+export default Services;
